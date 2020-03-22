6f8f0f2c3f61f159f187862ce67f1e77432233dc
maurosch <maurolschiavinato@gmail.com>
2020-03-22 16:45:38 -0300

    ordeno

--- todo/New Year Domino/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#define forn(i,n) for(int i = 0; i < int(n); i++)
-#define forsn(i,s,n) for(int i = int(s); i < int(n); i++)
-
-int main()
-{
-    return 0;
-}
